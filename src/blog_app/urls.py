28832e1f97aace15f1fac777ad9055068a944280
Nectar <piyushsingariya@gmail.com>
2020-02-18 16:53:51 +0530

    Added Create.html

--- src/blog_app/urls.py
+++ src/blog_app/urls.py
@@ -2,13 +2,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from posts.views import index, blog, post, search
+from posts.views import (index, blog, post,
+                         search, post_update,
+                         post_delete, post_create)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('blog/', blog, name='post-list'),
+    path('create/', post_create, name='post_create'),
     path('post/<id>/', post, name='post-detail'),
+    path('post/<id>/update/', post_update, name='post-update'),
+    path('post/<id>/delete/', post_delete, name='post-delete'),
     path('search/', search, name='search'),
     path('tinymce/', include('tinymce.urls')),
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -2,8 +2,9 @@ from django.db.models import Count, Q
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import Post
-from .forms import CommentForm
+from .forms import CommentForm, PostForm
 from marketing.models import Signup
+
 
 def search(request):
     """
@@ -13,21 +14,23 @@ def search(request):
     queryset = Post.objects.all()
     query = request.GET.get('q')
     if query:
-            queryset = queryset.filter(
-                Q(title__icontains=query) |
-                Q(overview__icontains=query)
-            ).distinct()
+        queryset = queryset.filter(
+            Q(title__icontains=query) |
+            Q(overview__icontains=query)
+        ).distinct()
     context = {
         'queryset': queryset
     }
     return render(request, 'search.html', context)
 
+
 def get_category_count():
-     queryset = Post \
-         .objects \
-         .values('categories__title') \
-         .annotate(Count('categories__title'))
-     return queryset
+    queryset = Post \
+        .objects \
+        .values('categories__title') \
+        .annotate(Count('categories__title'))
+    return queryset
+
 
 def index(request):
     featured_index = Post.objects.filter(featured=True)
@@ -47,8 +50,8 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
-def blog(request):
 
+def blog(request):
     category_count = get_category_count()
     post_list = Post.objects.all()
     latest_posts = Post.objects.order_by('-timestamp')[0:3]
@@ -72,6 +75,7 @@ def blog(request):
     }
     return render(request, 'blog.html', context)
 
+
 def post(request, id):
     category_count = get_category_count()
     post1 = get_object_or_404(Post, id=id)
@@ -93,3 +97,25 @@ def post(request, id):
         'form': form,
     }
     return render(request, 'post.html', context)
+
+
+def post_create(request):
+    form = PostForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect(reverse("post-detail", kwargs={
+                'id': form.instance.id
+            }))
+    context = {
+        'form': form,
+    }
+    return render(request, "post_create.html", context)
+
+
+def post_update(request, id):
+    pass
+
+
+def post_delete(request, id):
+    pass
